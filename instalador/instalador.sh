158d9ba39dda90ed07b4449f7f27fbc45aae4796
irvindmoreno <irvind.moreno@hotmail.com>
2016-04-03 17:37:13 -0500

    mongo

--- instalador/instalador.sh
+++ instalador/instalador.sh
@@ -158,6 +158,27 @@ htacceserror()
 	sudo a2enmod ssl
 	sudo service apache2 restart
 }
+mongo()
+{
+	sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+	echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+	sudo apt-get update
+	sudo apt-get install -y mongodb-org=3.0.6 mongodb-org-server=3.0.6 mongodb-org-shell=3.0.6 mongodb-org-mongos=3.0.6 mongodb-org-tools=3.0.6
+	echo "mongodb-org hold" | sudo dpkg --set-selections
+	echo "mongodb-org-server hold" | sudo dpkg --set-selections
+	echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+	echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+	echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+	sudo service mongod restart
+	sudo apt-get install php5-mongo
+	sudo subl /etc/php5/apache2/php.ini
+	echo "agregaesto: extension = mongo.so"
+	sleep 3m
+	sudo service apache2 restart
+	sudo pecl install mongo
+	sudo apt-get install php5-dev php5-cli php-pear
+	sudo pecl install mongo
+}
 inicio
 gitInstall
 crearLLavesParaGir
@@ -172,4 +193,5 @@ configurarMiMaquina
 wine
 nodejsnpm
 gulp
+mongo
 #composer
